6ac96068c68faedfde0d7c2ac4a8d1d3ccc8e627
Joao Megale <joaoluizlopesmegale@gmail.com>
2024-05-13 12:11:54 -0300

    comentarios

--- teste.c
+++ teste.c
@@ -3,14 +3,15 @@
 #include <pthread.h>
 #include <time.h>
 #include <unistd.h>
-// #include <linux/time.h>
 
 #define MAX_N 20
 
+// Struct Position: armazena uma posição e o tempo que será gasto nela
 typedef struct {
     int x, y, time;
 } Position;
 
+// Struct ThreadData: armazena informações sobre a thread
 typedef struct {
     int id;
     int group_id;
@@ -18,15 +19,18 @@ typedef struct {
     Position *positions;
 } ThreadData;
 
+// Struct Cell: armazena informações sobre quem está numa célula, para
+// podermos saber quando uma thread pode acessá-la ou não. Cada célula
+// terá uma variável de condição.
 typedef struct {
-    int group_id;  // Grupo atual na célula, -1 se vazia
-    pthread_cond_t cond;  // Variável de condição para controle de acesso
+    int group_id;
+    pthread_cond_t cond;
 } Cell;
 
 int N, n_threads;
-ThreadData threads[MAX_N];
-pthread_mutex_t board_mutexes[MAX_N][MAX_N];
-Cell board[MAX_N][MAX_N];
+ThreadData threads[MAX_N]; // Vetor de threads
+pthread_mutex_t board_mutexes[MAX_N][MAX_N]; // Matriz de mutexes, um para cada célula
+Cell board[MAX_N][MAX_N]; // Matriz de células
 
 void passa_tempo(int tid, int x, int y, int decimos) {
     struct timespec zzz, agora;
@@ -56,37 +60,50 @@ void passa_tempo(int tid, int x, int y, int decimos) {
 
 void *thread_function(void *arg) {
     ThreadData *data = (ThreadData *)arg;
+
+    // Guarda a posição inicial da thread
     int current_x = data->positions[0].x;
     int current_y = data->positions[0].y;
 
+    // Loop pelas posições que a thread vai percorrer
     for (int i = 0; i < data->num_positions; i++) {
+
+        // Pega a próxima célula
         int next_x = data->positions[i].x;
         int next_y = data->positions[i].y;
 
+        // Bloqueia o mutex da próxima célula, para poder verificar se ela está livre
         pthread_mutex_lock(&board_mutexes[next_x][next_y]);
 
-        // Espera até que a célula esteja livre ou ocupada por um grupo diferente
+        // Espera por um sinal até que a próxima célula esteja livre (group_id = -1) 
+        // ou ocupada por um grupo diferente
         while (board[next_x][next_y].group_id != -1 && board[next_x][next_y].group_id == data->group_id) {
             pthread_cond_wait(&board[next_x][next_y].cond, &board_mutexes[next_x][next_y]);
         }
 
-        // Movimentação para a célula
+        // Liberação da célula anterior - apenas se não for a primeira iteração
         if (i > 0) {
-            pthread_mutex_lock(&board_mutexes[current_x][current_y]);
-            board[current_x][current_y].group_id = -1;  // Libera a célula anterior
-            pthread_cond_broadcast(&board[current_x][current_y].cond);  // Notifica outras threads
-            pthread_mutex_unlock(&board_mutexes[current_x][current_y]);
+            pthread_mutex_lock(&board_mutexes[current_x][current_y]);   // Bloqueia o mutex para fazer alterações
+            board[current_x][current_y].group_id = -1;                  // Libera a célula anterior - (group_id = -1)
+            pthread_cond_broadcast(&board[current_x][current_y].cond);  // Notifica outras threads que a célula está livre agora
+            pthread_mutex_unlock(&board_mutexes[current_x][current_y]); // Desbloqueia o mutex
         }
 
-        board[next_x][next_y].group_id = data->group_id;  // Ocupa a nova célula
+        // Atualiza a célula com o grupo que está ocupando ela
+        board[next_x][next_y].group_id = data->group_id;
+
+        // Libera o mutex da célula em que a thread entrou
         pthread_mutex_unlock(&board_mutexes[next_x][next_y]);
         
+        // Atualiza a célula atual onde está a thread
         current_x = next_x;
         current_y = next_y;
+
+        // Executa a passa_tempo
         passa_tempo(data->id, current_x, current_y, data->positions[i].time);
     }
 
-    // Finalização na última posição
+    // Libera a última célula quando a thread termina sua rota
     pthread_mutex_lock(&board_mutexes[current_x][current_y]);
     board[current_x][current_y].group_id = -1;
     pthread_cond_broadcast(&board[current_x][current_y].cond);
@@ -99,6 +116,7 @@ int main() {
     scanf("%d %d", &N, &n_threads);
     pthread_t thread_ids[MAX_N];
 
+    // Obtém as informações das threads via input
     for (int i = 0; i < n_threads; i++) {
         scanf("%d %d %d", &threads[i].id, &threads[i].group_id, &threads[i].num_positions);
         threads[i].positions = malloc(threads[i].num_positions * sizeof(Position));
@@ -109,6 +127,7 @@ int main() {
 
     }
 
+    // Cria as threads
     for (int i = 0; i < n_threads; i++) {
         pthread_create(&thread_ids[i], NULL, thread_function, &threads[i]);
     }
